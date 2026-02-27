7720cd624eada889180d0fe3fb38052929d84b49
Juxest <juxest@qq.com>
2026-02-27 12:17:47 +0800

    Revert "fix(card): sync collapse watcher to avoid height snap on expand"
    This reverts commit 4d88ca4460f8eaa607b9a9d50216c382a2db54a3.

--- packages/components/card/src/card.vue
+++ packages/components/card/src/card.vue
@@ -50,10 +50,6 @@ watch(
       scheduleExpandUnlock()
     }
   },
-  {
-    // Ensure lock class is set in the same tick as collapse=false, before render.
-    flush: 'sync',
-  },
 )
 
 const onCollapseTransitionEnd = (event: TransitionEvent) => {
